49d19ee1d64391258788b4ac01c530ed11cd8fff
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2022-03-01 17:06:31 -0500

    CLASE 0

---
+++ HelloWorld/src/HelloWorld.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : Quino B. Jeffry
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+/***********************************************************/
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
